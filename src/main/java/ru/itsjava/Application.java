7fdc3899cadb979947c69f1ae9e12ec0f2697d1f
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2022-02-03 13:46:37 +0300

    Homework lesson 5

---
+++ src/main/java/ru/itsjava/Application.java
@@ -0,0 +1,19 @@
+package ru.itsjava;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import ru.itsjava.services.CoffeeHouse;
+
+@EnableAspectJAutoProxy
+@ComponentScan("ru.itsjava")
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Application.class);
+
+        CoffeeHouse coffeeHouse = context.getBean("coffeeHouseImpl", CoffeeHouse.class);
+        coffeeHouse.barista();
+    }
+}
